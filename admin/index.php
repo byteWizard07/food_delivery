e2a4633c764b5a40458ea7713fc3471d116353d7
Amit kumar <105741439+Limsoo-ho@users.noreply.github.com>
2024-02-27 00:47:03 +0530

    first commit

---
+++ admin/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+        <title>Food Order Website - Home Page</title>
+        <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <!-- Menu Section starts-->
+    <div class = "menu">
+        <div class="wrapper">Menu goes here</div>  
+    </div>
+    <!-- Menu section end-->
+
+    <!-- Main content Section starts-->
+    <div class="main-content">
+    <div class="wrapper">Main content goes here</div>
+    </div>
+    <!-- Main content section end-->
+
+    <!-- footer Section starts-->
+    <div class = "footer">
+    <div class="wrapper">Footer goes here</div>
+    </div>
+    <!-- footer section end-->
+
+</body>
+</html>
