21687edf7141f794113795925813ec91a4c249fb
Amit kumar <105741439+Limsoo-ho@users.noreply.github.com>
2024-02-27 15:06:18 +0530

    second commit

--- admin/index.php
+++ admin/index.php
@@ -5,20 +5,56 @@
 </head>
 <body>
     <!-- Menu Section starts-->
-    <div class = "menu">
-        <div class="wrapper">Menu goes here</div>  
+    <div class = "menu text-center">
+        <div class="wrapper">
+         <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Admin</a></li>
+            <li><a href="#">Category</a></li>
+            <li><a href="#">Food</a></li>
+            <li><a href="#">Order</a></li>
+         </ul>
+        </div>  
     </div>
     <!-- Menu section end-->
 
     <!-- Main content Section starts-->
     <div class="main-content">
-    <div class="wrapper">Main Content goes here</div>
+    <div class="wrapper">
+        <h1>Dashboard</h1>
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+
+        <div class="col-4 text-center">
+            <h1>5</h1>
+            <br>
+            Categories
+        </div>
+        <div class="clearfix"></div>
+    </div>
     </div>
     <!-- Main content section end-->
 
     <!-- footer Section starts-->
     <div class = "footer">
-    <div class="wrapper">Footer goes here</div>
+    <div class="wrapper">
+        <p class="text-center">2024 All rights reserved, Developed by - <a href="https://www.linkedin.com/in/amit-kumar-0874b7217/">Amit Kumar</a></p>
+    </div>
     </div>
     <!-- footer section end-->
 
